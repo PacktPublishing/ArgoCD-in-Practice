e5bb7fbfde814fd94e75ddd933651acb301f3cc2
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-12-13 15:52:30 +0200

    Fix

--- ch05/terraform/argocd.tf
+++ ch05/terraform/argocd.tf
@@ -1,12 +1,3 @@
-data "template_file" "bootstrap" {
-  template = file("../k8s-bootstrap/base/master-utilities.yaml")
-  vars = {
-    iam_role   = aws_iam_role.external_dns.arn
-    domain     = var.domain
-    txtOwnerId = var.zone_id
-  }
-}
-
 data "kustomization_build" "argocd" {
   path = "../k8s-bootstrap/bootstrap"
 }
@@ -14,7 +5,4 @@ data "kustomization_build" "argocd" {
 resource "kustomization_resource" "argocd" {
   for_each = data.kustomization_build.argocd.ids
   manifest = data.kustomization_build.argocd.manifests[each.value]
-  depends_on = [
-    data.template_file.bootstrap
-  ]
 }
